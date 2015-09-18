e64ead650cb07e36752a619135894bc8ca32e2db
mtgNic <nic.jsb@gmail.com>
2015-09-18 17:36:50 -0400

    setting up D3 structure
    CSV loads correctly and populates data object

--- js/script.js
+++ js/script.js
@@ -3,5 +3,38 @@ var margin = {top: 60, right: 20, bottom: 30, left: 40},
     height = 600 - margin.top - margin.bottom;
 
 
+var x = d3.scale.linear()
+    .range([0, width]);
+
 var y = d3.scale.linear()
     .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient('left');
+
+// need to add d3 tip
+
+var svg = d3.select('#chart').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+d3.csv('data/child-mortality-long.csv', function(error, data) {
+
+    // Coerce values to numeric
+    data.forEach(function(d) {
+        console.log(d);
+        d.Country   = +d.Country;
+        d.Value     = +d.Value;
+        d.Year      = +d.Year;
+    })
+
+
+
+});
