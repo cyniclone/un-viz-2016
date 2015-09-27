56412cd523715a52963a98b2ea54c7a4d7dade77
mtgNic <nic.jsb@gmail.com>
2015-09-27 13:33:37 -0400

    Added drop-down invoked activation function

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-//function addActive(selector) {
-//    d3.select(selector).classed('active', true);
-//}
-//
-//function removeActive(selector) {
-//    d3.select(selector).classed('active', false);
-//}
