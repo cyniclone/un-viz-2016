632d3634fe81dbc22b8bebc33140b801b6013c11
mtgNic <nic.jsb@gmail.com>
2015-12-25 14:38:24 -0500

    cleaning up directories

--- js/helpers.js
+++ js/helpers.js
@@ -1,4 +1,4 @@
-// Util functions
+/***** Control mouseover events ************/
 function activate(selector) {
     selector.classed("active", true);
     selector.moveToFront();
@@ -12,5 +12,32 @@ function deactivate(selector) {
 d3.selection.prototype.moveToFront = function() {
     return this.each(function(){
         this.parentNode.appendChild(this);
+    });
+};
+
+
+/***** Populate Static Content ************/
+// Populate nav
+function populateSelect(path) {
+    d3.csv(path, function(error, data) {
+        var select = d3.select("#select-country").append("select").attr("id","dropdown");
+
+        select.on("change", function() {
+            deactivate(d3.selectAll(".scatterdot"));
+            deactivate(d3.selectAll(".scatterdot2"));
+
+            var element = d3.select(this).property("value");
+            activate(d3.selectAll(element));
+        });
+
+        $('#dropdown').html("<option>Select a country</option>");
+
+        // Add a select option for each country
+        select.selectAll("option")
+            .data(data)
+            .enter()
+            .append("option")
+            .attr("value", function (d) { return ".c" + d.hash; })
+            .text(function (d) { return d.CountryName; });
     });
 };
